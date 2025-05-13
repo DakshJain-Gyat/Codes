02129afdba728c9a46255eb776f7b14e978a8fc6
DakshJain-Gyat <awdredredred3@gmail.com>
2025-05-13 12:31:58 +0530

    My first project

---
+++ script (5).js
@@ -0,0 +1,425 @@
+let songs = [
+  {
+    "title": "We Don't Care",
+    "artist": "Kanye West",
+    "album": "The College Dropout",
+    "date": "2004-02-10",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/We%20Don%27t%20Care.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Graduation Day",
+    "artist": "Kanye West",
+    "album": "The College Dropout",
+    "date": "2004-02-10",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Graduation%20Day.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "All Falls Down",
+    "artist": "Kanye West",
+    "album": "The College Dropout",
+    "date": "2004-02-10",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/All%20Falls%20Down.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Spaceship",
+    "artist": "Kanye West",
+    "album": "The College Dropout",
+    "date": "2004-02-10",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Spaceship.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Jesus Walks",
+    "artist": "Kanye West",
+    "album": "The College Dropout",
+    "date": "2004-02-10",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Jesus%20Walks.mp3",
+    "vibe": "epic"
+  },
+  {
+    "title": "Never Let Me Down",
+    "artist": "Kanye West",
+    "album": "The College Dropout",
+    "date": "2004-02-10",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Never%20Let%20Me%20Down.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Get Em High",
+    "artist": "Kanye West",
+    "album": "The College Dropout",
+    "date": "2004-02-10",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Get%20Em%20High.mp3",
+    "vibe": "energetic"
+  },
+  {
+    "title": "Workout Plan",
+    "artist": "Kanye West",
+    "album": "The College Dropout",
+    "date": "2004-02-10",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Workout%20Plan.mp3",
+    "vibe": "fun"
+  },
+  {
+    "title": "The New Workout Plan",
+    "artist": "Kanye West",
+    "album": "The College Dropout",
+    "date": "2004-02-10",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/The%20New%20Workout%20Plan.mp3",
+    "vibe": "fun"
+  },
+  {
+    "title": "Slow Jamz",
+    "artist": "Kanye West",
+    "album": "The College Dropout",
+    "date": "2004-02-10",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Slow%20Jamz.mp3",
+    "vibe": "romantic"
+  },
+  {
+    "title": "Breathe In Breathe Out",
+    "artist": "Kanye West",
+    "album": "The College Dropout",
+    "date": "2004-02-10",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Breathe%20In%20Breathe%20Out.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "School Spirit",
+    "artist": "Kanye West",
+    "album": "The College Dropout",
+    "date": "2004-02-10",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/School%20Spirit.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Two Words",
+    "artist": "Kanye West",
+    "album": "The College Dropout",
+    "date": "2004-02-10",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Two%20Words.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Through The Wire",
+    "artist": "Kanye West",
+    "album": "The College Dropout",
+    "date": "2004-02-10",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Through%20The%20Wire.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Family Business",
+    "artist": "Kanye West",
+    "album": "The College Dropout",
+    "date": "2004-02-10",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Family%20Business.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Last Call",
+    "artist": "Kanye West",
+    "album": "The College Dropout",
+    "date": "2004-02-10",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Last%20Call.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Wake Up Mr. West",
+    "artist": "Kanye West",
+    "album": "Late Registration",
+    "date": "2005-08-30",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Wake%20Up%20Mr.%20West.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Heard 'Em Say",
+    "artist": "Kanye West",
+    "album": "Late Registration",
+    "date": "2005-08-30",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Heard%20%27Em%20Say.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Touch The Sky",
+    "artist": "Kanye West",
+    "album": "Late Registration",
+    "date": "2005-08-30",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Touch%20The%20Sky.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Gold Digger",
+    "artist": "Kanye West",
+    "album": "Late Registration",
+    "date": "2005-08-30",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Gold%20Digger.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Drive Slow",
+    "artist": "Kanye West",
+    "album": "Late Registration",
+    "date": "2005-08-30",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Drive%20Slow.mp3",
+    "vibe": "romantic"
+  },
+  {
+    "title": "My Way Home",
+    "artist": "Kanye West",
+    "album": "Late Registration",
+    "date": "2005-08-30",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/My%20Way%20Home.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Crack Music",
+    "artist": "Kanye West",
+    "album": "Late Registration",
+    "date": "2005-08-30",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Crack%20Music.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Roses",
+    "artist": "Kanye West",
+    "album": "Late Registration",
+    "date": "2005-08-30",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Roses.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Bring Me Down",
+    "artist": "Kanye West",
+    "album": "Late Registration",
+    "date": "2005-08-30",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Bring%20Me%20Down.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Addiction",
+    "artist": "Kanye West",
+    "album": "Late Registration",
+    "date": "2005-08-30",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Addiction.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Diamonds from Sierra Leone",
+    "artist": "Kanye West",
+    "album": "Late Registration",
+    "date": "2005-08-30",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Diamonds%20from%20Sierra%20Leone.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "We Major",
+    "artist": "Kanye West",
+    "album": "Late Registration",
+    "date": "2005-08-30",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/We%20Major.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Hey Mama",
+    "artist": "Kanye West",
+    "album": "Late Registration",
+    "date": "2005-08-30",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Hey%20Mama.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Gone",
+    "artist": "Kanye West",
+    "album": "Late Registration",
+    "date": "2005-08-30",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Gone.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Good Morning",
+    "artist": "Kanye West",
+    "album": "Graduation",
+    "date": "2007-09-11",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Good%20Morning.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Stronger",
+    "artist": "Kanye West",
+    "album": "Graduation",
+    "date": "2007-09-11",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Stronger.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Good Life",
+    "artist": "Kanye West",
+    "album": "Graduation",
+    "date": "2007-09-11",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Good%20Life.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Can't Tell Me Nothing",
+    "artist": "Kanye West",
+    "album": "Graduation",
+    "date": "2007-09-11",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Can%27t%20Tell%20Me%20Nothing.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Flashing Lights",
+    "artist": "Kanye West",
+    "album": "Graduation",
+    "date": "2007-09-11",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Flashing%20Lights.mp3",
+    "vibe": "epic"
+  },
+  {
+    "title": "Homecoming",
+    "artist": "Kanye West",
+    "album": "Graduation",
+    "date": "2007-09-11",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Homecoming.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Big Brother",
+    "artist": "Kanye West",
+    "album": "Graduation",
+    "date": "2007-09-11",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Big%20Brother.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Welcome To Heartbreak",
+    "artist": "Kanye West",
+    "album": "808s & Heartbreak",
+    "date": "2008-11-24",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Welcome%20To%20Heartbreak.mp3",
+    "vibe": "romantic"
+  },
+  {
+    "title": "Heartless",
+    "artist": "Kanye West",
+    "album": "808s & Heartbreak",
+    "date": "2008-11-24",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Heartless.mp3",
+    "vibe": "romantic"
+  },
+  {
+    "title": "Love Lockdown",
+    "artist": "Kanye West",
+    "album": "808s & Heartbreak",
+    "date": "2008-11-24",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Love%20Lockdown.mp3",
+    "vibe": "romantic"
+  },
+  {
+    "title": "Paranoid",
+    "artist": "Kanye West",
+    "album": "808s & Heartbreak",
+    "date": "2008-11-24",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Paranoid.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Street Lights",
+    "artist": "Kanye West",
+    "album": "808s & Heartbreak",
+    "date": "2008-11-24",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Street%20Lights.mp3",
+    "vibe": "epic"
+  },
+  {
+    "title": "Runaway",
+    "artist": "Kanye West",
+    "album": "My Beautiful Dark Twisted Fantasy",
+    "date": "2010-11-22",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Runaway.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Devil In A New Dress",
+    "artist": "Kanye West",
+    "album": "My Beautiful Dark Twisted Fantasy",
+    "date": "2010-11-22",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Devil%20In%20A%20New%20Dress.mp3",
+    "vibe": "dark"
+  },
+  {
+    "title": "Power",
+    "artist": "Kanye West",
+    "album": "My Beautiful Dark Twisted Fantasy",
+    "date": "2010-11-22",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Power.mp3",
+    "vibe": "soulful"
+  },
+  {
+    "title": "Blame Game",
+    "artist": "Kanye West",
+    "album": "My Beautiful Dark Twisted Fantasy",
+    "date": "2010-11-22",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Blame%20Game.mp3",
+    "vibe": "dark"
+  },
+  {
+    "title": "Who Will Survive In America",
+    "artist": "Kanye West",
+    "album": "My Beautiful Dark Twisted Fantasy",
+    "date": "2010-11-22",
+    "duration": null,
+    "file": "https://pub-d47cc282c24c41478652f06a92ac958f.r2.dev/Who%20Will%20Survive%20In%20America.mp3",
+    "vibe": "soulful"
+  }
+];
